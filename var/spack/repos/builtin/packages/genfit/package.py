a391053cf4abc89d9989cd4304402bc19982918b
Dmitri Smirnov <dmixsmi@gmail.com>
2021-01-13 21:16:16 -0500

    genfit: Define new version, update dependency
    - Add dependency on eigen package
    - Add last version known to work with root <6.18
    
      The GenFit project lacks any release or tag versions, therefore, we are forced
      to use date versions for now
    
    - Update title wording and default archive type (tar.gz compression prevails over zip)

--- var/spack/repos/builtin/packages/genfit/package.py
+++ var/spack/repos/builtin/packages/genfit/package.py
@@ -7,14 +7,19 @@ from spack import *
 
 
 class Genfit(CMakePackage):
-    """GenFit is a tracking framework in particle and nuclear physics."""
+    """GenFit is an experiment-independent framework for track reconstruction in
+        particle and nuclear physics"""
 
     homepage = "https://github.com/GenFit/GenFit"
-    url      = "https://github.com/GenFit/GenFit/archive/master.zip"
+    url      = "https://github.com/GenFit/GenFit/archive/master.tar.gz"
     git      = "https://github.com/GenFit/GenFit.git"
 
     maintainers = ['mirguest']
 
     version('master', branch='master')
+    version('2017-06-23', commit='b496504a739382e8043c38c893d8df6eed20e560')
 
     depends_on('root')
+    depends_on('eigen')
+
+    conflicts('root@6.18.00:', when='@2017-06-23')
